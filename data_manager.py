ed153bdf79488fd223fcf6013027609331f69068
Aw <walia_77@outlook.com>
2022-09-27 12:05:17 -0400

    Added sheety API methods for GET and POST

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,43 @@
+import requests
+
+
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self, url):
+        self.sheet_url = url
+
+    def sheet_action(self, action_type, **kwargs):
+        sheet_inputs = {}
+        auth = ()
+        data = None
+        for key, value in kwargs.items():
+            if key == 'json':
+                sheet_inputs = value
+            if key == 'auth':
+                auth = value
+        if action_type == 'GET':
+            # Get Sheet data
+            response = requests.get(self.sheet_url)
+            response.raise_for_status()
+            data = response.json()
+            return data
+        elif action_type == 'POST':
+            # Check if entry there already in spreadsheet
+            response = requests.get(self.sheet_url)
+            response.raise_for_status()
+            data = response.json()
+            dest_exist = False
+            for x in data['prices']:
+                if x['city'] == sheet_inputs['price']['city']:
+                    print("Destination already there")
+                    dest_exist = True
+
+            if dest_exist == False:
+                # Post sheet data
+                sheet_response = requests.post(self.sheet_url, json=sheet_inputs, auth=auth)
+                sheet_result = sheet_response.json()
+                print(f"New destination added in google sheet{sheet_result}")
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+
+import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+from data_manager import DataManager
+
+load_dotenv()
+
+SHEET_URL = "https://api.sheety.co/fba4dfbb939ba81e50bb949091994b8f/flightDeals/prices"
+SHEET_USERNAME = os.environ.get('SHEET_USERNAME')
+SHEET_PASSWORD = os.environ.get('SHEET_PASSWORD')
+auth = (SHEET_USERNAME, SHEET_PASSWORD)
+
+sheet1 = DataManager(SHEET_URL)
+# Get sheet data
+#
+
+
+sheet_input = {
+    'price': {
+        'city': "Test",
+        'iataCode': 1234,
+        'lowestPrice': 12
+    }
+}
+# Add destination to sheet data
+# Check if entry there if not there then only add
+data = sheet1.sheet_action('GET')
+print(f"data is {data}")
+print(f"sheet input is {sheet_input['price']['city']}")
+
+sheet1.sheet_action('POST', json=sheet_input, auth=auth )
+#
+### url = "https://api.tequila.kiwi.com/v2/search"
+#
+##API_KEY = os.environ.get('API_KEY') parameters = {
+#     'fly_from':'YYZ',
+#     'fly_to': 'YVR',
+#     'dateFrom': '01/11/2022',
+#     'dateTo': '04/11/2022',
+#     'curr': 'CAD'
+# }
+#
+# headers = {
+#     "apikey": API_KEY,
+#     "Content-Type": "application/json"
+# }
+#
+# response = requests.get(url, parameters, headers=headers)
+# response.raise_for_status()
+# data = response.json()
+# print(data)
